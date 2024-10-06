4608acd62e53f3979ab4573484469d9c13af7abc
edog6 <41647777+edog6@users.noreply.github.com>
2024-10-06 14:25:42 +0200

    Update for-loop for creating the grid
    The squares are not effected by the size of the browser window.
    Now an additional div with the class "gird-row" will be created
    indicating a new row for the grid.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,13 @@
 const container = document.getElementById("grid-container");
 
 // Creating the total amount of squares
-for (let i = 1; i <= 16*16; i++) {
-    const square = document.createElement("div");
-    square.classList.add("grid-item");
-    container.appendChild(square);
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.classList.add("grid-row");
+    for (let j = 0; j < 16; j++) {
+        const square = document.createElement("div");
+        square.classList.add("grid-item");
+        row.appendChild(square);
+    }
+    container.appendChild(row);
 }
